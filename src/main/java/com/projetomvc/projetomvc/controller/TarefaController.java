fed62c80e84bf188fd6d268f8c8dcb425ef75f20
João Wictor Tessmann <joaowictortessmann97@gmail.com>
2025-10-14 21:04:20 -0300

    feat: Adiciona o controlador responsavel pelas acoes de listar e adicionar

--- src/main/java/com/projetomvc/projetomvc/controller/TarefaController.java
+++ src/main/java/com/projetomvc/projetomvc/controller/TarefaController.java
@@ -1,5 +1,39 @@
 package com.projetomvc.projetomvc.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.projetomvc.projetomvc.model.Tarefa;
+import com.projetomvc.projetomvc.repository.TarefaRepository;
+import org.springframework.ui.Model;
+
+@Controller
 public class TarefaController {
-    
+
+    @Autowired // Injeção de dependência do repositório
+    private TarefaRepository tarefaRepository;
+
+    @GetMapping("/")
+    public String listarTarefas(Model model, @RequestParam(defaultValue = "0") int page) {
+        int tamanhoPagina = 5;
+        Pageable configuracaoPagina = PageRequest.of(page, tamanhoPagina);
+        Page<Tarefa> paginaTarefas = tarefaRepository.findAll(configuracaoPagina);
+        model.addAttribute("paginaTarefas", paginaTarefas);
+        model.addAttribute("novaTarefa", new Tarefa());
+        return "index";
+    }
+
+    @PostMapping("/adicionar")
+    public String adicionarTarefa(@ModelAttribute Tarefa novaTarefa) {
+        tarefaRepository.save(novaTarefa);
+        return "redirect:/";
+    }
 }
