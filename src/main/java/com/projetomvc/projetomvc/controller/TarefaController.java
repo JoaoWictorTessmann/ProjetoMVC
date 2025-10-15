132b75fbff6585f63d11b5bdb5013c376615d1a4
João Wictor Tessmann <joaowictortessmann97@gmail.com>
2025-10-14 21:06:02 -0300

    feat: Adiciona o controlador responsavel pelas acoes de listar e adicionar

--- src/main/java/com/projetomvc/projetomvc/controller/TarefaController.java
+++ src/main/java/com/projetomvc/projetomvc/controller/TarefaController.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
